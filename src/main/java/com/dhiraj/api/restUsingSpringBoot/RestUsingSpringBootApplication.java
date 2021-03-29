b92f4b44630f5b873a5e1bf6e50ecbca14da0429
dhirajs <dhiraj.surve@openet.com>
2021-03-29 19:01:13 -0400

    first

---
+++ src/main/java/com/dhiraj/api/restUsingSpringBoot/RestUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dhiraj.api.restUsingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestUsingSpringBootApplication.class, args);
+	}
+
+}
